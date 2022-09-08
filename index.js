e964950538745d03473973f814dfc1395b3ad8ee
jaquel <jaquelrey@gmail.com>
2022-09-08 01:38:00 -0400

    fixed formatting, mvp is complete

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
-const generateLicense = require('./utils/generateLicense')
+const generateLicense = require('./utils/generateLicense');
+const licenseMarkdown = require('./utils/licenseMarkdown')
+
+
 
 
 //questions array included, many are optional unless truly needed
@@ -14,6 +17,7 @@ function promptUser() {
             message: 'What is the name of your project?',
             validate: function (answer) {
                 if (answer.length < 1) {
+
                     return console.log("You must enter a title for your project.");
                 }
                 return true;
@@ -25,6 +29,7 @@ function promptUser() {
             message: 'Give a short description of your project, and why you built it.',
             validate: function (answer) {
                 if (answer.length < 1) {
+
                     return console.log("You must enter a description for your project.");
                 }
                 return true;
@@ -84,7 +89,7 @@ function promptUser() {
         {
             type: 'list',
             name: 'license',
-            message: 'Pick a liscense for this project, if desired. For more information, visit https://choosealicense.com/',
+            message: 'Pick a license for this project, if desired. For more information, visit https://choosealicense.com/ ....',
             choices: ['MIT License', 'ISC License', 'GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'The Unlicense']
         },
         {
@@ -96,31 +101,96 @@ function promptUser() {
             type: 'input',
             name: 'email',
             message: 'Enter your email address.',
-        },
+        }
+        /*{   for future development
+            type: 'confirm',
+             name: 'licensemd',
+            message: 'Would you like to generate an additional .md of your selected license?',
+         },*/
     ]);
 };
 
-// async init 
-async function init(){
-    try {
-        // inquirer questions
-        const data = await promptUser();
+/*   for future development
 
-        //section that gets selected license (if exists)
-        //and then returns sheild.io badge to pass to md gen
-        let badge
+function licenseQuestion(license){
 
-        if(data.license){
-            badge = generateLiscense(data.license)
-            generateMarkdown(data, badge)
-        } else {
-            generateMarkdown(data, badge)    
+    //called during the README generation process if user has selected a license
+    console.log("You've selected to generate an additional file: license.md");
+    console.log("If you'd like the license.md to include your name instead of a placeholder, answer this prompt.");
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'fullname',
+            message: 'What\'s your full name?',
         }
+    ]);
+} */
+
+
+// async init 
+async function init() {
+    try {
+        // await response from inquirer questions
+        const data = await promptUser();
+        const license = data.license
+        const filename = (data.title).split(' ').join('-')
 
         console.log("Responses recieved....");
 
-    } catch(error) {
-        console.log(error);
+        //if user has chosen to use a license:
+        //get selected license and pass to generateLicense
+        //return shield.io badge
+        let badge
+
+        if (license) {
+            console.log("Creating " + filename + ".md....");
+            badge = generateLicense(license);
+            let finalMd = generateMarkdown(data, badge);
+
+            fs.writeFile("./created/" + filename + ".md", finalMd, function (error) {
+                if (error) throw error;
+                console.log("Success!");
+            });
+
+            /* for future development (generates complete license file)
+
+            //if with a nested async await, nested in an if else inside of an async await.......
+
+            if (data.licensemd === true){
+                async function init2(){
+                    try {
+                        //grabbing the current year to add to license
+
+                        const year = new Date().getFullYear();
+                        
+                        //if user elects to generate a license.md, prompt user for their name (optional)
+
+                        const answer = await licenseQuestion(); 
+                        const chosenMd = licenseMarkdown(license, answer, year);
+
+                        //call licenseMarkdown, which passes back md
+                        //license md is saved as a seperate file in same directory as README
+                        
+                    } catch (error) {
+                        console.log("Can\'t generate license.md : " + error);
+                    }
+                    init2();
+                }
+            }*/
+
+        } else {
+
+            let finalMd = generateMarkdown(data);
+            console.log("Creating " + filename + ".md....");
+
+            fs.writeFile("./created/" + filename + ".md", finalMd, function (error) {
+                if (error) throw error;
+                console.log("Success!");
+            });
+        }
+    } catch (error) {
+        console.log("Can\'t generate README.md : " + error);
     }
 };
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,186 +1,138 @@
-// importing license generation to be added 
-const generateLicense = require('generateLicense')
-
-
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-
+function generateMarkdown(data, badge) {
   // generating table of contents based on returned data from prompts
 
-  let contents = `## Table of Contents`;
-
+let contents = `## Table of Contents  
+`;
   if (data.features || data.technologies || data.dependancies) {
-    contents +=
-    `* [Details](#details)`
-  };
-
+    contents += `* [Details](#details)  
+`;
+  }
   if (data.features) {
-    contents +=
-    `*    [Features](#features)`
-  };
-
+    contents += `       * [Features](#features)  
+`;
+  }
+  if (data.technologies) {
+    contents += `       * [Technologies](#technologies)  
+`;
+  }
+  if (data.dependancies) {
+    contents += `       * [Dependancies](#dependancies)  
+`;
+  }
   if (data.installation) {
-    contents +=
-    `* [Installation](#installation)`
-  };
-
+    contents += `* [Installation](#installation)  
+`;
+  }
   if (data.usage) {
-    contents +=
-    `* [Usage](#usage)`
-  };
-
+    contents += `* [Usage](#usage)  
+`;
+  }
   if (data.contributing) {
-    contents +=
-    `* [Contributing](#contributing)`
-  };
-
+    contents += `* [Contributing](#contributing)  
+`;
+  }
   if (data.tests) {
-    contents +=
-    `* [Tests](#tests)`
-  };
-
+    contents += `* [Tests](#tests)  
+`;
+  }
   if (data.github || data.email) {
-    contents +=
-    `* [Questions](#questions)`
-  };
+    contents += `* [Questions](#questions)  
+`;
+  }
 
-  contents += `* [Credits](#credits)`
-
-
+  contents += `* [Credits](#credits)  
+`;
 
   //generating head
+  let mdHead = `# ${data.title}  
+${badge}  
+## Description  
+${data.description}  
+`;
 
-  let mdHead =
-
-    `# ${data.title}
-
-  ${badge}
-  
-  ## Description
-
-  >
-  >${data.description}
-  >  
-    
-  `;
-
-  // add generated head and ToC 
-
-  mdHead += contents;
+  // add generated head and ToC
+  let mdTop = (mdHead += contents);
 
   //generate sections and their content (if they exist)
-
-  let mdBody = ` --- 
-  `
-
+  let mdBody = `----  
+`;
   if (data.features || data.technologies || data.dependancies) {
-    mdBody +=
-    `##Details  
-      
-    `
-  };
-
-
+    mdBody += `## Details  
+`;
+  }
   if (data.features) {
-    mdBody +=
-    `###Features  
-      
-    ${data.features}
-      
-    `
-  };
-
+    mdBody += `### Features  
+${data.features}  
+`;
+  }
   if (data.technologies) {
-    mdBody +=
-    `###Technologies  
-      
-    ${data.technologies}
-      
-    `
-  };
-
+    mdBody += `### Technologies  
+${data.technologies}  
+`;
+  }
   if (data.dependancies) {
-    mdBody +=
-    `###Features  
-      
-    ${data.dependancies}
-      
-    `
-  };
-
+    mdBody += `### Dependancies  
+${data.dependancies}  
+  
+----  
+  `;
+  }
   if (data.installation) {
-    mdBody +=
-    `##Installation  
-      
-    ${data.installation}
-      
-    `
-  };
-
+    mdBody += `  
+## Installation  
+${data.installation}  
+`;
+  }
   if (data.usage) {
-    mdBody +=
-    `##Usage  
-      
-    ${data.usage}
-      
-    `
-  };
-
+    mdBody += `## Usage  
+${data.usage}  
+  
+----  
+  `;
+  }
   if (data.contributing) {
-    mdBody +=
-    `##Contributing  
-      
-    ${data.contributing}
-      
-    `
-  };
-
+    mdBody += `## Contributing  
+${data.contributing}  
+`;
+  }
   if (data.tests) {
-    mdBody +=
-    `##Tests  
-      
-    ${data.tests}
-      
-    `
-  };
-
+    mdBody += `## Tests  
+${data.tests}  
+`;
+  }
   if (data.github || data.email) {
-    contents +=
-    `##Questions    
-    >  
-    >Got questions? Reach out to me at:  
-    >`
+    mdBody += `## Questions  
+  
+>Got questions? Reach out to me at:  
+`;
   }
 
   if (data.github) {
-    mdBody +=
-    `My [Github](https://github.com/${data.github})    
-
-    `
-  };
-
-  if (data.email) {
-    mdBody +=
-    `Email: [${data.email}](mailto:${data.email})    
-    
-    `
-  };
-
-  mdBody+=
-  `##Credits  
-  
-  ###Authors  
-    
-  ${data.authors}
-    
-  `
-  if (data.credits) {
-    mdBody +=
-    `###Resources  
-      
-    ${data.credits}`
+    mdBody += `>My [Github](https://github.com/${data.github})  
+`;
   }
 
+  if (data.email) {
+    mdBody += `>Email: [${data.email}](mailto:${data.email})  
+  
+----  
+  `;
+  }
+
+  mdBody += `## Credits  
+### Authors  
+${data.authors}  
+`;
+  if (data.credits) {
+    mdBody += `### Resources  
+${data.credits}  
+`;
+  }
+
+  let finMd = (mdTop += mdBody);
+
+  return finMd;
 }
 
 module.exports = generateMarkdown;
